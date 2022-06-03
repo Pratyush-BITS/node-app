91f337ad0519b8eb53ea4f7af6c8a7aa82deb7cb
Pratyush Sinha <pratyush.sinha01@sap.com>
2022-06-04 03:55:09 +0530

    message update

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get('/', function (req, res) {
      "An authentic and traditional south indian noodles recipe made with rice flour in a traditional steam cooker. \n" +
      "it basically hails from the kerala cuisine of south india, but is hugely popular across india and are mainly \n" +
      "served for morning breakfast. it is generally served with a coconut-based mild curry known as vegetable stew or \n" +
-     "locally known as ‘istew’ but can be served with any chutney or any spicy curry."
+     "locally known as 'stew' but can be served with any chutney or any spicy curry."
     res.send(recipies);
 });
 //listen to port 3000 by default
--- test/test.js
+++ test/test.js
@@ -8,8 +8,13 @@ describe('node-app', () => {
 
     describe('GET /', function() {
         it('respond with hello There', (done) => {
+            let recipies = "idiyappam recipe | string hopper recipe | noolappam | instant idiyappam with stew recipe.\n" +
+     "An authentic and traditional south indian noodles recipe made with rice flour in a traditional steam cooker. \n" +
+     "it basically hails from the kerala cuisine of south india, but is hugely popular across india and are mainly \n" +
+     "served for morning breakfast. it is generally served with a coconut-based mild curry known as vegetable stew or \n" +
+     "locally known as 'stew' but can be served with any chutney or any spicy curry."
             //navigate to root and check the response is "hello There"
-            request(app).get('/').expect('hello There').end(done);
+            request(app).get('/').expect(recipies).end(done);
         });
     });
 })
